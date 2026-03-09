d839505aeb895056c8323863ac19d197cf06ffea
Phoenicopterus <dyagilev.sergey.spb@gmail.com>
2026-03-09 23:25:31 +0300

    Fix database.py file

--- database.py
+++ database.py
@@ -23,8 +23,6 @@ if not DATABASE_URL:
     DB_NAME = os.getenv('DB_NAME')
     DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
 engine = create_engine(DATABASE_URL)
 
 
