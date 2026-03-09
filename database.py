81cbffc68244e96b7d5b9f7abd25536c37e134d5
Phoenicopterus <dyagilev.sergey.spb@gmail.com>
2026-03-09 23:17:52 +0300

    Fix database.py file

--- database.py
+++ database.py
@@ -13,6 +13,15 @@ DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
+DATABASE_URL = os.get_env('DATABSE_URL')
+
+if not DATABASE_URL:
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+    DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
